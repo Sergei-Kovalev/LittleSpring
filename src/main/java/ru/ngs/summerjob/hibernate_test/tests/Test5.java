bacef3d7f0760a146dc65b78dfec06e8a7ad6bcd
Sergei Kovalev <summerjob@ngs.ru>
2022-12-05 18:40:26 +0300

    Hibernate... update & delete

---
+++ src/main/java/ru/ngs/summerjob/hibernate_test/tests/Test5.java
@@ -0,0 +1,37 @@
+package ru.ngs.summerjob.hibernate_test.tests;
+
+import jakarta.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.ngs.summerjob.hibernate_test.entity.Employee;
+
+import java.util.List;
+
+public class Test5 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            Employee employee = session.get(Employee.class, 0);
+//            session.remove(employee);
+
+            List<Employee> resultList = session.createQuery("from Employee WHERE name = 'Oleg'", Employee.class).getResultList();
+            for (Employee e : resultList) {
+                session.remove(e);
+            }
+
+            session.getTransaction().commit();
+
+            System.out.println("DONE!!!");
+        } finally {
+            factory.close();
+        }
+    }
+}
